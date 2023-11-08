31d9053579325e970b1734b4cff373f905042187
Kristina Marjak <kristina@directo.ee>
2023-11-08 17:08:12 +0200

    homework1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import cover from './cover.jpg';
 import './App.css';
+import './Description.js'
+import Description from './Description.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={cover} className="App-cover" alt="cover" />
+        <h2><b>1984</b></h2>
+        <p> George Orwell, Thomas Pynchon <i>(Foreword)</i></p>
+
+        &#9830;	&#9830;	&#9830;	&#9830;	
+
+        <Description/>
+          
+          <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.goodreads.com/book/show/61439040-1984"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read more on Goodreads
         </a>
       </header>
     </div>
+    
   );
 }
 
---
+++ src/Description.js
@@ -0,0 +1,15 @@
+function Description() {
+    return (
+      <div className="App-text">
+        <p>
+            The new novel by George Orwell is the major work towards which all his previous writing has pointed. Critics have hailed it as his "most solid, most brilliant" work. Though the story of Nineteen Eighty-Four takes place thirty-five years hence, it is in every sense timely. The scene is London, where there has been no new housing since 1950 and where the city-wide slums are called Victory Mansions. Science has abandoned Man for the State. As every citizen knows only too well, war is peace.
+        </p>
+        <p>
+            To Winston Smith, a young man who works in the Ministry of Truth (Minitru for short), come two people who transform this life completely. One is Julia, whom he meets after she hands him a slip reading, "I love you." The other is O'Brien, who tells him, "We shall meet in the place where there is no darkness." The way in which Winston is betrayed by the one and, against his own desires and instincts, ultimately betrays the other, makes a story of mounting drama and suspense.
+        </p>
+      </div>
+    );
+  }
+
+
+export default Description;
